97c86c08b568c343c474cf1806c4d6c3a09aa839
Linus Falk <linusfalk@Linuss-MacBook-Pro.local>
2022-02-06 16:01:19 +0100

    empty routes home and tetris

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <div></div>
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Tetris from './pages/Tetris'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/tetris' element={<Tetris />} />
+    </Routes>
+  )
 }
 
 export default App
